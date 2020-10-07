3bcc7261f859cc2f0edeba95549e4c81291114f5
AdemTEN <marrysnm2012@gmal.com>
2020-10-07 15:17:53 +0200

    commit

--- src/test/java/com/cybertek/pages/VehicleContractsPage.java
+++ src/test/java/com/cybertek/pages/VehicleContractsPage.java
@@ -12,11 +12,18 @@ public class VehicleContractsPage extends BasePage {
     @FindBy(xpath = "//i[contains(text(),'Next')]")
     public WebElement nextButton;
 
-    @FindBy(xpath = "//label[contains(text(),'of 2 |')]")
+   // @FindBy(xpath = "//label[contains(text(),'of 2 |')]")
+    @FindBy(xpath = "//label[contains(text(),'of')]" )
     public WebElement numberOfPage;
 
     @FindBy(xpath = "//tbody")
     public WebElement allrows;
+
+    @FindBy(xpath = "(//*[@href='javascript:void(0);'])[5]")
+    public WebElement threepoint;
+
+    @FindBy(xpath = "//body/ul[1]/li[1]/ul[1]/li[2]/a[1]/i[1]")
+    public WebElement editIcon;
 
 
 
--- src/test/java/com/cybertek/tests/Exercises/VyTrack/TC_144_Edit.java
+++ src/test/java/com/cybertek/tests/Exercises/VyTrack/TC_144_Edit.java
@@ -1,33 +1,57 @@
 package com.cybertek.tests.Exercises.VyTrack;
 
+import com.cybertek.pages.CreateVehicleContractPage;
+import com.cybertek.pages.GeneralInformationPage;
+import com.cybertek.pages.VehicleContractsPage;
 import com.cybertek.tests.TestBase;
 import com.cybertek.tests.TestBase2ForExercise;
 import com.cybertek.utilities.BrowserUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC_144_Edit extends TestBase2ForExercise {
+import java.util.Arrays;
+
+public class TC_144_Edit extends TestBase {
 
     @Test
     public  void tc144() throws InterruptedException {
-        WebElement threepoint = driver.findElement(By.xpath("//*[@href='javascript:void(0);'][1]"));
-        Thread.sleep(6000);
-        BrowserUtils.hover(threepoint);
+        VehicleContractsPage vehicleContractsPage = new VehicleContractsPage();
+
+        String numberOfPage = vehicleContractsPage.numberOfPage.getText();
+
+        String [] pagenumber = numberOfPage.split(" ");
+
+        System.out.println("Arrays.toString(pagenumber) = " + Arrays.toString(pagenumber));
+
+        int number = Integer.parseInt(pagenumber[1]);
+        System.out.println("number = " + number);
+        extentLogger.info("Click the next Button 'number of page -1' times");
+
+        for (int i = 1; i < number ; i++) {
+            BrowserUtils.clickWithJS(vehicleContractsPage.nextButton);
+        }
+        vehicleContractsPage.waitUntilLoaderScreenDisappear();
+
+        BrowserUtils.hover(vehicleContractsPage.threepoint);
+        BrowserUtils.waitFor(2);
+        BrowserUtils.clickWithJS(vehicleContractsPage.editIcon);
+
+        CreateVehicleContractPage createVehicleContractPage = new CreateVehicleContractPage();
+        createVehicleContractPage.responsible.clear();
+        createVehicleContractPage.responsible.sendKeys("Schumaher");
+
+        BrowserUtils.clickWithJS(createVehicleContractPage.saveAndCloseButton);
+
+        GeneralInformationPage generalInformationPage = new GeneralInformationPage();
+        BrowserUtils.waitFor(2);
+        String header = "Schumaher Mclaren Hamilton 25250";
+        Assert.assertTrue(generalInformationPage.getHeaderOfContract(header).getText().contains("Schumaher"));
+        Assert.assertEquals(generalInformationPage.nameOfResponsible.getText(),"Schumaher");
 
 
-
-        WebElement edit = driver.findElement(By.cssSelector("[class='fa-pencil-square-o hide-text']"));
-        BrowserUtils.waitForStaleElement(edit);
-        BrowserUtils.clickWithJS(edit);
-
-
-        //driver.findElement(By.cssSelector("[name='custom_entity_type[Responsible]']")).clear();
-        //driver.findElement(By.cssSelector("[name='custom_entity_type[Responsible]']")).sendKeys("Marco");
-        //Save and close
-        //WebElement saveButton = driver.findElement(By.cssSelector(".btn.btn-success.action-button"));
-        //jse.executeScript("arguments[0].click();",saveButton);
     }
 
 
